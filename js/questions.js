1332c71d2a2accb828e49c4d3a8fb2c712418cd1
Vikas Gavandi <30316401+DIGILABTEAM@users.noreply.github.com>
2021-09-29 18:00:17 +0530

    Deployment of Project
    Deployment of Project New UI with v.10

---
+++ js/questions.js
@@ -0,0 +1,46 @@
+/*All questions in the quiz goes here in the data object.....*/
+var data =
+	[
+		[
+			{
+				question: "Which bronchial secretion enhancer is approved by U.S FDA?",
+				option1: "Guaiphenesin",
+				option2: "Potassium iodide",
+				option3: "Ammonium chloride",
+				option4: "Potassium citrate",
+				answer: 1
+			},
+			{
+				question: "Which drug is synthetic central NMDA(N-methyl-D-aspartate) receptor antagonist?",
+				option1: "Noscapine",
+				option2: "Chlophedianol",
+				option3: "Dextromethorphan",
+				option4: "Prenoxydiazine",
+				answer: 3
+			},
+			{
+				question: "Which drug cause nausea, drowsiness, dizziness and at higher dose cause hallucination, ataxia may occur",
+				option1: "Noscapine",
+				option2: "Chlophedianol",
+				option3: "Dextromethorphan",
+				option4: "Prenoxydiazine",
+				answer: 3
+			},
+			{
+				question: "What are three potential causes of a productive cough?",
+				option1: "Bronchitis",
+				option2: "Bacterial infection",
+				option3: "Anaerobic bacterial infection",
+				option4: "All of the above",
+				answer: 4
+			},
+			{
+				question: "Dry, centrally painful, non-productive cough points to which of the following condition",
+				option1: "Pneumonia",
+				option2: "tracheitis",
+				option3: "both A and B",
+				option4: "None of the above",
+				answer: 3
+			}
+		]
+	];
